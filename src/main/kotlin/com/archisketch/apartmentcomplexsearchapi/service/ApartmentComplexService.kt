0ceedc045ca030876ec066953e55a2e059c0efdb
Sean-creative <ksw6125000@naver.com>
2024-04-06 03:19:51 +0900

    feat: ApartmentComplexService 기본 메소드작성
    로직은 없고, 리턴값은 하드코딩
    
    Close #2

--- src/main/kotlin/com/archisketch/apartmentcomplexsearchapi/service/ApartmentComplexService.kt
+++ src/main/kotlin/com/archisketch/apartmentcomplexsearchapi/service/ApartmentComplexService.kt
@@ -1,10 +1,40 @@
 package com.archisketch.apartmentcomplexsearchapi.service
 
+import com.archisketch.apartmentcomplexsearchapi.dto.AddressDto
+import com.archisketch.apartmentcomplexsearchapi.dto.ApartmentComplexDto
+import com.archisketch.apartmentcomplexsearchapi.dto.FloorPlanDto
+import org.springframework.data.domain.Sort
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 @Transactional
 class ApartmentComplexService {
+    val addresses = listOf(AddressDto(": 서울특별시 강동구 상암로 224, 상암로 225", "서울특별시 강동구 상일동 519, 상일동 520"))
+    val floorPlans = listOf(FloorPlanDto("112B", true, 1.0, 1.0, "" ))
 
+    fun getApartmentComplexByName(name: String): ApartmentComplexDto? {
+
+        return ApartmentComplexDto(1L, "sean", addresses, floorPlans)
+    }
+
+    fun getApartmentComplexesByRoadAddress(roadAddress: String): List<ApartmentComplexDto> {
+
+        return listOf(ApartmentComplexDto(1L, "sean", addresses, floorPlans))
+    }
+
+    fun getApartmentComplexesByJibunAddress(jibunAddress: String): List<ApartmentComplexDto> {
+
+        return listOf(ApartmentComplexDto(1L, "sean", addresses, floorPlans))
+    }
+
+    fun getFloorPlansByComplexNameAndType(name: String, type: String, sortDirection: Sort.Direction): List<FloorPlanDto> {
+
+        return floorPlans
+    }
+
+    fun getFloorPlansByComplexNameAndExpansion(name: String, isExpanded: Boolean, sortDirection: Sort.Direction): List<FloorPlanDto> {
+
+        return floorPlans
+    }
 }
