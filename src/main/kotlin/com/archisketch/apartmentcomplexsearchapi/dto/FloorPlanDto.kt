7efde57699c10ce75e42f004d0c7de67b667890e
Sean-creative <ksw6125000@naver.com>
2024-04-06 02:39:20 +0900

    refactor: ApartmentDto.kt로 관련 DTO를 모음
    편의성을 위해 ApartmentDto로 관련 Dto를 모음
    
    Close #2

--- src/main/kotlin/com/archisketch/apartmentcomplexsearchapi/dto/FloorPlanDto.kt
+++
@@ -1,9 +0,0 @@
-package com.archisketch.apartmentcomplexsearchapi.dto
-
-data class FloorPlanDto(
-    val type: String,
-    val isExpanded: Boolean,
-    val exclusiveArea: Double,
-    val supplyArea: Double,
-    val imageUrl: String
-)
