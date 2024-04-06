6b5b2e05663725a849a54e438e73896853e9a92b
Sean-creative <ksw6125000@naver.com>
2024-04-06 21:40:26 +0900

    feat: 3.조회 API 개발 (/sigungu) 개발 완료
    조회 API 개발 (/sigungu) 개발 및 포스트맨 확인 완료
    
    Close #6

--- src/main/kotlin/com/archisketch/apartmentcomplexsearchapi/controller/SigunguController.kt
+++ src/main/kotlin/com/archisketch/apartmentcomplexsearchapi/controller/SigunguController.kt
@@ -1,3 +1,17 @@
 package com.archisketch.apartmentcomplexsearchapi.controller
 
-class SigunguController {}
+import com.archisketch.apartmentcomplexsearchapi.dto.SigunguDTO
+import com.archisketch.apartmentcomplexsearchapi.entity.Sigungu
+import com.archisketch.apartmentcomplexsearchapi.service.SigunguService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class SigunguController(val sigunguService: SigunguService) {
+
+    @GetMapping("/sigungu/{pidLocCode}")
+    fun getSigungu(@PathVariable pidLocCode: Long): List<SigunguDTO> {
+        return sigunguService.getSigunguByPidLocCode(pidLocCode)
+    }
+}
